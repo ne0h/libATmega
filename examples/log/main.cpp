d0d64c95b948c9d86362f15dc4d9b4de824ba1f5
Maximilian Hess <mail@ne0h.de>
2019-12-31 14:42:52 +0100

    [Log] Improve API and add example

---
+++ examples/log/main.cpp
@@ -0,0 +1,9 @@
+#include <log.hpp>
+
+int main() {
+    Log::getInstance()->log("Starting...%d...0x%x...%d...", 1, 255, 10000);
+    Log::getInstance()->log(512, "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Ste");
+
+    while (1) {}
+    return 0;
+}
--- log.hpp
+++ log.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdio.h>
+
 #include <usart.hpp>
 
 // source: https://www.avrfreaks.net/forum/avr-c-micro-how?name=PNphpBB2&file=viewtopic&t=59453
@@ -20,6 +22,10 @@ void operator delete(void* ptr, unsigned int i) {
     free(ptr);
 } 
 
+/**
+ * Log is a very simple logging framework which leverages USART0.
+ * Log is implemented as singelton.
+ */
 class Log {
 
 private:
@@ -30,6 +36,14 @@ private:
     Log() {}
     Log(const Log&);
     ~Log() {}
+
+    void vlog(size_t bufSize, const char* format, va_list args) {
+        char buf[bufSize];
+        vsprintf(buf, format, args);
+
+        m_usart.send(buf);
+        m_usart.send('\n');
+    }
 
     class CGuard {
         public:
@@ -42,6 +56,10 @@ private:
     };
 
 public:
+    /**
+     * Returns a pointer to the instance of Log.
+     * \return a pointer to the instance of Log.
+     */
     static Log* getInstance() {
         static CGuard g;
         if (!m_instance) {
@@ -50,10 +68,28 @@ public:
         return m_instance;
     }
 
-    void info(char* message) {
-        m_usart.send("[INFO] ");
-        m_usart.send(message);
-        m_usart.send('\n');
+    /**
+     * Adds a new line to the log. Makes use of sprintf() to format the result.
+     * Uses a pre-defined length of 128 to format the result.
+     * \param format The format for the result.
+     */
+    void log(const char* format, ...) {
+        va_list args;
+        va_start(args, format);
+        vlog(128, format, args);
+        va_end(args);
+    }
+
+    /**
+     * Adds a new line to the log. Makes use of sprintf() to format the result.
+     * \param bufSize The size of the buffer used to format the result.
+     * \param format The format for the result.
+     */
+    void log(size_t bufSize, const char* format, ...) {
+        va_list args;
+        va_start(args, format);
+        vlog(bufSize, format, args);
+        va_end(args);
     }
 
 };

f7727e0adc2a713096f83b5b8302fff23338fed2
Maximilian Hess <mail@ne0h.de>
2020-02-22 21:34:35 +0100

    Move operators and guards to common.hpp

--- led.hpp
+++ led.hpp
@@ -4,26 +4,10 @@
 #include <stdlib.h>
 #include <avr/io.h>
 
+#include <common.hpp>
+
 const static uint16_t TIMER8_MAX  = 255;
 const static uint16_t TIMER16_MAX = 1999;
-
-// source: https://www.avrfreaks.net/forum/avr-c-micro-how?name=PNphpBB2&file=viewtopic&t=59453
-
-__extension__ typedef int __guard __attribute__((mode (__DI__)));
-
-extern "C" int __cxa_guard_acquire(__guard *);
-extern "C" void __cxa_guard_release (__guard *);
-
-int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);};
-void __cxa_guard_release (__guard *g) {*(char *)g = 1;};
-
-void* operator new(size_t size) {
-    return malloc(size);
-}
-
-void operator delete(void* ptr, unsigned int i) {
-    free(ptr);
-}
 
 /**
  * LEDId identifies a LED.
--- log.hpp
+++ log.hpp
@@ -4,24 +4,7 @@
 #include <stdlib.h>
 
 #include <usart.hpp>
-
-// source: https://www.avrfreaks.net/forum/avr-c-micro-how?name=PNphpBB2&file=viewtopic&t=59453
-
-__extension__ typedef int __guard __attribute__((mode (__DI__)));
-
-extern "C" int __cxa_guard_acquire(__guard *);
-extern "C" void __cxa_guard_release (__guard *);
-
-int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);};
-void __cxa_guard_release (__guard *g) {*(char *)g = 1;};
-
-void* operator new(size_t size) {
-    return malloc(size);
-}
-
-void operator delete(void* ptr, unsigned int i) {
-    free(ptr);
-} 
+#include <common.hpp>
 
 /**
  * Log is a very simple logging framework which leverages USART0.
